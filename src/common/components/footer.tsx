1c9cd95f88313bf46239e75768f2785afa376e7b
dan-speekl <dan@speekl.com>
2025-07-24 21:01:02 +0200

    Add Product Hunt badge to Footer component for Traque promotion

--- src/common/components/footer.tsx
+++ src/common/components/footer.tsx
@@ -44,6 +44,20 @@ export function Footer() {
                 Monitoring and error tracking for your startup.
               </span>
             </h2>
+
+            <a
+              href="https://www.producthunt.com/products/traque?embed=true&utm_source=badge-featured&utm_medium=badge&utm_source=badge-traque"
+              target="_blank"
+              className="mt-8"
+            >
+              <img
+                src="https://api.producthunt.com/widgets/embed-image/v1/featured.svg?post_id=997067&theme=dark&t=1753383471340"
+                alt="Traque - Error&#0032;Tracking&#0032;Service&#0032;for&#0032;Your&#0032;Startup | Product Hunt"
+                style={{ width: '250px', height: '54px' }}
+                width="250"
+                height="54"
+              />
+            </a>
           </div>
 
           <div className="grid grid-cols-1 gap-8 justify-self-end md:grid-cols-4">
